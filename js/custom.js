7b863ca1018592bd332115b5c7b1e67fba0eed2a
Emiliano Liotta <liotta.emiliano@gmail.com>
2016-04-28 17:49:40 -0300

    'user_edit' view added. 'user_profile' removed. Also 'custom.js' was added.

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$("#change-pass-btn").on('click',function(){
+		//Cuando se hace click en el boton, se muestra o se oculta el formulario.
+		$("#change-pass-form").slideToggle(); 
+	});
+
+});
