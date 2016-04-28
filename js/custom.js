0b0e0db193c7d3bff2bf4e19cd70306090cdccbe
lucashour <lucashour1993@gmail.com>
2016-04-28 19:15:33 -0300

    Minor changes on custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 
-	$("#change-pass-btn").on('click',function(){
-		//Cuando se hace click en el boton, se muestra o se oculta el formulario.
+	// USER EDIT - Botón de cambio de contraseña
+	$("#change-pass-btn").on('click', function(){
+
+		var button = $("#change-pass-btn");
+		var button_text = button.text();
+
+		if (button_text == "CANCELAR CAMBIO DE CONTRASEÑA")
+			button_text = "¿CAMBIAR CONTRASEÑA?";
+		else
+			button_text = "CANCELAR CAMBIO DE CONTRASEÑA";
+
+		//Cuando se hace click en el botón, se muestra o se oculta el formulario y se cambia el texto del mismo.
+		$("#change-pass-btn").text(button_text);
 		$("#change-pass-form").slideToggle();
 	});
 
