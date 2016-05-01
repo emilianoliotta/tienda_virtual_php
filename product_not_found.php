7db3654bec16c0433fd8579446f858d359756a66
lucashour <lucashour1993@gmail.com>
2016-05-01 14:41:25 -0300

    'Product not found' page added

---
+++ product_not_found.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+	<?php include_once("head.php"); ?>
+	<body>
+
+		<?php
+			include_once("messages.php");
+			include_once("header.php");
+		?>
+
+		<!-- CUERPO -->
+
+		<div id="cuerpo">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 col-md-10 col-md-offset-1">
+						<div class="panel centrar">
+							<br/>
+							<div class="container">
+								<div class="row centrar">
+                  <div class="error">Producto no encontrado</div>
+                  <hr>
+                  <a href="products.php" class="button">VOLVER A NAVEGACIÓN</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--FIN del CUERPO -->
+
+		<?php include_once("footer.php"); ?>
+
+	</body>
+</html>
